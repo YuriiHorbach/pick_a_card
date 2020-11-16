2cbfa79a314daffde1a6c7f667f20baf53325922
YuriiHorbach <gyrii84@gmail.com>
2020-11-17 00:06:16 +0200

    Add unicode symbols to cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,7 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ["\u2666", "\u2665", "\u2663", "\u2660"]
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample.encode('utf-8')}"
+#
+# checkmark = "\u2666"
+# puts checkmark.encode('utf-8')
